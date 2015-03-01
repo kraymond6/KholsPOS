fc2624563c14467e7e26845310a54fd9c2f9aeef
Kallie <Kallie@Kallies_Laptop>
2015-02-28 23:37:08 -0600

    Sixth commit: continued to create methods for cash register class

--- src/kholspos/CashRegister.java
+++ src/kholspos/CashRegister.java
@@ -8,12 +8,17 @@ package kholspos;
 public class CashRegister {
     public String customerId;
     public String date;
+    Receipt receipt =  new Receipt(date, customerId);
     public CashRegister(){
-        
+ 
     }
+  
     public void beginTransaction(String custId, String date){
         customerId = custId;
         this.date = date;
-        Receipt newReceipt =  new Receipt(date, customerId);
+    }
+    
+    public void addItem(Product item){
+        receipt.addItem(item);
     }
 }
